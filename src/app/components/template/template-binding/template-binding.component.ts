84cd9d8acd8685db5f7bdb2decb6dfb222fe4b50
BrendonAraujo <brendong.araujo@gmail.com>
2025-01-13 08:21:03 -0300

    #ADD - Template Binding

--- src/app/components/template/template-binding/template-binding.component.ts
+++ src/app/components/template/template-binding/template-binding.component.ts
@@ -10,6 +10,15 @@ import { Component } from '@angular/core';
 export class TemplateBindingComponent {
   public name : string = "Brendon Araújo";
   public age : number = 23;
+  public isDisable : boolean = true;
+  public srcValue : string = "https://yt3.googleusercontent.com/4z0oRVDPoyuGeYAXx4ctU1xAoVSdrqtENmIoWQZSd4MQJkizoMVsOdoN5_Qp9n0duQXsFo_w=s160-c-k-c0x00ffffff-no-rj";
+  public imgName : string = "image"
+
+  constructor(){
+    // setTimeout(() => {
+    //   this.name = "Raul Seixas"
+    // }, 1000);
+  }
 
   public sum(firstValue : number, secondNumber : number){
     return firstValue + secondNumber;
